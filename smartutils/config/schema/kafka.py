455e7289b0739880b41139b71d414db06681787c
wuruimiao <dev.wuruimiao@qq.com>
2025-04-27 14:43:18 +0800

    refactor: mq

--- smartutils/config/schema/kafka.py
+++ smartutils/config/schema/kafka.py
@@ -1,6 +1,6 @@
 from typing import List, Literal
 
-from pydantic import BaseModel, field_validator, conlist
+from pydantic import BaseModel, field_validator, conlist, conint
 
 from smartutils.config.schema.host import HostConf
 
@@ -10,24 +10,10 @@ class KafkaConf(BaseModel):
     client_id: str
     acks: Literal['all', 1, 0] = 'all'
     compression_type: Literal['zstd', 'snappy', None] = None
-    max_batch_size: int = 16384
-    linger_ms: int = 100
-    request_timeout_ms: int = 5000
-    retry_backoff_ms: int = 300
-
-    @field_validator('max_batch_size')
-    @classmethod
-    def batch_size_positive(cls, v):
-        if v <= 0:
-            raise ValueError('max_batch_size must be positive')
-        return v
-
-    @field_validator('linger_ms', 'request_timeout_ms', 'retry_backoff_ms')
-    @classmethod
-    def must_be_non_negative(cls, v, info):
-        if v < 0:
-            raise ValueError(f'{info.field_name} must be non-negative')
-        return v
+    max_batch_size: conint(gt=0) = 16384
+    linger_ms: conint(ge=0) = 0
+    request_timeout_ms: conint(gt=0) = 40000
+    retry_backoff_ms: conint(gt=0) = 100
 
     @field_validator('client_id')
     @classmethod
@@ -39,3 +25,8 @@ class KafkaConf(BaseModel):
     @property
     def urls(self) -> List[str]:
         return [item.the_url for item in self.bootstrap_servers]
+
+    @property
+    def kw(self) -> dict:
+        params = self.model_dump(exclude={'bootstrap_servers'})
+        return params
