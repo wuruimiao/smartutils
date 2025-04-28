9491696b7045409810464a2f2efcff2952ab7d53
wuruimiao <dev.wuruimiao@qq.com>
2025-04-28 11:05:14 +0800

    feat: project conf

---
+++ tests/config/schema/test_project.py
@@ -0,0 +1,33 @@
+import pytest
+from pydantic import ValidationError
+
+from smartutils.config.schema.project import ProjectConf
+
+@pytest.mark.parametrize(
+    "name, description, version",
+    [
+        ("unmanned", "无人柜审核", "0.0.1"),
+        ("auth", "权限", "0.0.2"),
+    ]
+)
+def test_project_conf_valid(name, description, version):
+    conf = ProjectConf(name=name, description=description, version=version)
+    assert conf.name == name
+    assert conf.description == description
+    assert conf.version == version
+
+@pytest.mark.parametrize(
+    "name, description, version",
+    [
+        ("unmanned", "", " "),
+        ("", "无人柜", None),
+        ("   ", " ", "   "),
+        (1, 2, 3),
+        (None, None, None),
+    ]
+)
+def test_project_conf_invalid(name, description, version):
+    with pytest.raises(ValidationError) as exc:
+        ProjectConf(name=name, description=description, version=version)
+    assert ('Input should be a valid string' in str(exc.value)
+            or 'String should have at least 1 character' in str(exc.value))
