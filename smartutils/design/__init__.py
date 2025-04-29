1a2e433a28bd49ebd4181e9b55b2a20d28d70ef0
wuruimiao <dev.wuruimiao@qq.com>
2025-04-29 16:13:11 +0800

    ✨ feat(deprecated): add deprecated decorator in design module

--- smartutils/design/__init__.py
+++ smartutils/design/__init__.py
@@ -1,7 +1,10 @@
 from smartutils.design.singleton import singleton, SingletonBase, SingletonMeta
+from smartutils.design.deprecated import deprecated
 
 __all__ = [
     'singleton',
     'SingletonBase',
     'SingletonMeta',
+
+    'deprecated',
 ]
---
+++ smartutils/design/deprecated.py
@@ -0,0 +1,18 @@
+import warnings
+import functools
+
+
+def deprecated(new_method):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(
+                f"{func.__name__} is deprecated. Use {new_method} instead.",
+                DeprecationWarning,
+                stacklevel=2
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
