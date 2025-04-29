601cc265ce68e29e83f5ce7752d80d761aefd3e2
wuruimiao <dev.wuruimiao@qq.com>
2025-04-29 18:04:37 +0800

    fix: app

--- smartutils/app/fast/app.py
+++ smartutils/app/fast/app.py
@@ -1,43 +1,42 @@
-from contextlib import asynccontextmanager
+def create_app():
+    from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-from smartutils.app.fast.middlewares import HeaderMiddleware
-from smartutils.ret import ResponseModel
+    from fastapi import FastAPI
+    from smartutils.app.fast.middlewares import HeaderMiddleware
+    from smartutils.ret import ResponseModel
 
+    @asynccontextmanager
+    async def lifespan(_app: FastAPI):
+        from smartutils.config import init, get_config
+        init()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    from smartutils.config import init, get_config
-    init()
+        from smartutils.log import init
+        init()
 
-    from smartutils.log import init
-    init()
+        from smartutils.infra import init
+        await init()
 
-    from smartutils.infra import init
-    await init()
+        from smartutils.ID import SnowflakeGenerator
+        _app.state.gen = SnowflakeGenerator(instance=get_config().project.id)
 
-    from smartutils.ID import SnowflakeGenerator
-    app.state.gen = SnowflakeGenerator(instance=get_config().project.id)
+        yield
 
-    yield
+        from smartutils.log import logger
+        logger.info(f'shutdown start close')
+        from smartutils.infra.manager import ResourceManagerRegistry
+        await ResourceManagerRegistry.close_all()
+        logger.info(f'shutdown all closed')
 
-    from smartutils.log import logger
-    logger.info(f'shutdown start close')
-    from smartutils.infra.manager import ResourceManagerRegistry
-    await ResourceManagerRegistry.close_all()
-    logger.info(f'shutdown all closed')
+    app = FastAPI(lifespan=lifespan)
 
+    app.add_middleware(HeaderMiddleware)
 
-app = FastAPI(lifespan=lifespan)
+    @app.get('/')
+    def root() -> ResponseModel:
+        return ResponseModel()
 
-app.add_middleware(HeaderMiddleware)
+    @app.get('/healthy')
+    def healthy() -> ResponseModel:
+        return ResponseModel()
 
-
-@app.get('/')
-def root() -> ResponseModel:
-    return ResponseModel()
-
-
-@app.get('/healthy')
-def healthy() -> ResponseModel:
-    return ResponseModel()
+    return app
