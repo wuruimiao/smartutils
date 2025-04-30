54f8e2ccc8c398883d5d47ba441c6665f1e4df4d
wuruimiao <dev.wuruimiao@qq.com>
2025-04-30 08:26:16 +0800

    refactor: remove unused

--- smartutils/infra/abstract.py
+++ smartutils/infra/abstract.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Callable, Any, Awaitable, TypeVar, Generic, AsyncContextManager
+from typing import TypeVar, AsyncContextManager
 
 
 class AbstractResource(ABC):
