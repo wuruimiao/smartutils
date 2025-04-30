e8ebef4aea1707cde026182de443d5cb51f045b7
wuruimiao <dev.wuruimiao@qq.com>
2025-04-30 13:54:50 +0800

    feat: log middleware

--- smartutils/app/fast/middlewares.py
+++ smartutils/app/fast/middlewares.py
@@ -1,9 +1,11 @@
-from fastapi import Request
+from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from smartutils.app.fast.header import CustomHeader
 from smartutils.app.const import HEADERKeys
 from smartutils.ctx import CTXKeys, CTXVarManager
+from smartutils.log import logger
+from smartutils.timer import Timer
 
 
 class HeaderMiddleware(BaseHTTPMiddleware):
@@ -24,3 +26,17 @@ class HeaderMiddleware(BaseHTTPMiddleware):
 
             response.headers[HEADERKeys.X_TRACE_ID] = trace_id
             return response
+
+
+class LoggMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        async with Timer() as t:
+            response: Response = await call_next(request)
+        client_host = request.client.host if request.client else "-"
+        logger.info(
+            f"{client_host} - '{request.method} {request.url}' - "
+            f"Query: {dict(request.query_params)} "
+            f"Status: {response.status_code} - "
+            f"Cost: {t.elapsed:.3f} sec"
+        )
+        return response
