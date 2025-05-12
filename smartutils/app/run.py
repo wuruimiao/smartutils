5e77abe09428963dcb24f8435d36bf4a5f8f6ba0
wuruimiao <dev.wuruimiao@qq.com>
2025-05-12 11:53:14 +0800

    ♻️ refactor(app): improve create_app and run logic, add test features

--- smartutils/app/run.py
+++ smartutils/app/run.py
@@ -1,12 +1,23 @@
 import argparse
 import uvicorn
 
+from smartutils.app.fast.app import create_app
 
-def start():
+# TODO: 兼容多种应用，不限于fastapi
+
+
+def run():
     parser = argparse.ArgumentParser(description="Run app/main")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="Host to bind")
     parser.add_argument("--port", type=int, default=80, help="Port to bind")
     parser.add_argument("--reload", action="store_false", help="Enable auto-reload")
     args = parser.parse_args()
 
-    uvicorn.run("app.main:app", host=args.host, port=args.port, reload=args.reload, workers=1, factory=True)
+    uvicorn.run(
+        create_app,
+        host=args.host,
+        port=args.port,
+        reload=args.reload,
+        workers=1,
+        factory=True,
+    )
