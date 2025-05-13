5da8e70b1e910aa526aef51b8e277f3f9291d1d1
wuruimiao <dev.wuruimiao@qq.com>
2025-05-13 15:29:16 +0800

    refactor: IDGen from BaseFactory

--- smartutils/ID/init.py
+++ smartutils/ID/init.py
@@ -1,35 +1,30 @@
-from typing import Optional, Dict, Type, Tuple, Callable
+from typing import Optional, Type, Tuple, Callable
 
 from smartutils.ID.abstract import AbstractIDGenerator
 from smartutils.ID.const import IDGenType
-from smartutils.call import exit_on_fail
-from smartutils.log import logger
+from smartutils.design import BaseFactory
+from smartutils.error.sys_err import LibraryUsageError
 
 
-class _IDGen(AbstractIDGenerator):
+class _IDGen(AbstractIDGenerator, BaseFactory[IDGenType, Tuple[Callable[..., AbstractIDGenerator], bool]]):
     def __init__(self):
         self._gen: Optional[AbstractIDGenerator] = None
         self._type: Optional[IDGenType] = None
-        self._registry: Dict[IDGenType, Tuple[Callable[..., AbstractIDGenerator], bool]] = {}
 
-    def register(self, id_type: IDGenType, need_conf: bool = False):
+    @classmethod
+    def register(cls, id_type: IDGenType, need_conf: bool = False):
         def decorator(gen_cls: Type[AbstractIDGenerator]) -> Type[AbstractIDGenerator]:
-            if id_type in self._registry:
-                logger.error("_IDGen key {key} already registered.", key=id_type)
-                exit_on_fail()
-            self._registry[id_type] = (gen_cls, need_conf)
+            super(_IDGen, cls).register(id_type)((gen_cls, need_conf))
             return gen_cls
 
         return decorator
 
     def init(self, id_gen_type: IDGenType = IDGenType.ULID, conf=None):
-        if id_gen_type not in self._registry:
-            raise ValueError(f"IDGen {id_gen_type} not registered")
+        gen_cls, need_conf = self.get(id_gen_type)
         self._type = id_gen_type
-        gen_cls, need_conf = self._registry[id_gen_type]
         if need_conf:
             if not conf:
-                raise ValueError(f"IDGen {gen_cls} need conf")
+                raise LibraryUsageError(f"IDGen {gen_cls} need conf")
             self._gen = gen_cls(**conf.kw)
         else:
             self._gen = gen_cls()
