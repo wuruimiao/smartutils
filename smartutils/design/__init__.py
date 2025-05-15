609fe9a7a1e698a14cb74eda773d70b579d78823
wuruimiao <dev.wuruimiao@qq.com>
2025-05-15 13:28:30 +0800

    feat: factory register order

--- smartutils/design/__init__.py
+++ smartutils/design/__init__.py
@@ -1,6 +1,7 @@
 from smartutils.design.deprecated import deprecated
 from smartutils.design.factory import BaseFactory
 from smartutils.design.singleton import singleton, SingletonBase, SingletonMeta
+from smartutils.design.attr import RequireAttrs
 
 __all__ = [
     "singleton",
@@ -10,4 +11,6 @@ __all__ = [
     "deprecated",
 
     "BaseFactory",
+
+    "RequireAttrs",
 ]
