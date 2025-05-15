fba190e0b91deeba1b2c3d958d196d65eb750bf2
wuruimiao <dev.wuruimiao@qq.com>
2025-05-15 17:49:58 +0800

    ♻️ refactor(init): improve __init__ import structure and add project metadata

--- smartutils/app/__init__.py
+++ smartutils/app/__init__.py
@@ -1,11 +1,12 @@
-from smartutils.app import adapter, plugin
+from smartutils.app import adapter as _adapter, plugin as _plugin
 from smartutils.app.req_ctx import ReqCTX
 from smartutils.app.run import run
 from smartutils.call import register_package
 from smartutils.ctx import CTXVarManager, CTXKey
+from smartutils.app.factory import AppHook
 
-register_package(adapter)
-register_package(plugin)
+register_package(_adapter)
+register_package(_plugin)
 
 CTXVarManager.register(CTXKey.USERID)(None)
 CTXVarManager.register(CTXKey.USERNAME)(None)
@@ -14,4 +15,5 @@ CTXVarManager.register(CTXKey.TRACE_ID)(None)
 __all__ = [
     "ReqCTX",
     "run",
+    "AppHook",
 ]
