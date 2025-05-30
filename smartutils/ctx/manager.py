dc0ed023805e4bd8f9224e13c4bb478fd73e39d7
wuruimiao <dev.wuruimiao@qq.com>
2025-05-30 10:21:34 +0800

    ✨ feat(ctx): synchronize CTXVarManager context keys usage

--- smartutils/ctx/manager.py
+++ smartutils/ctx/manager.py
@@ -13,12 +13,24 @@ class CTXVarManager(BaseFactory[CTXKey, contextvars.ContextVar]):
     @classmethod
     @contextmanager
     def use(cls, key: CTXKey, value: Any):
+        # 嵌套场景下，内层 value 会临时覆盖 contextvar 的 value，内层退出后自动恢复外层 value
+        # 增加is_first，嵌套场景下，都是同一个value
+
         var = super(CTXVarManager, cls).get(key)
-        token = var.set(value)
+        is_first = False
         try:
+            var.get()
+        except LookupError:
+            is_first = True
+
+        if is_first:
+            token = var.set(value)
+            try:
+                yield
+            finally:
+                var.reset(token)
+        else:
             yield
-        finally:
-            var.reset(token)
 
     @classmethod
     def get(cls, key: CTXKey, default: Any = None) -> Any:
