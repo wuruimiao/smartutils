e3c778a04368761444f31bfe096600935e5722cf
wuruimiao <dev.wuruimiao@qq.com>
2025-07-18 16:03:10 +0800

    ♻️ refactor(infra): update InfraFactory key types for clarity

--- smartutils/infra/factory.py
+++ smartutils/infra/factory.py
@@ -1,13 +1,14 @@
 from typing import Any, Callable, Tuple
 
+from smartutils.config.const import ConfKey
 from smartutils.design import BaseFactory
 
 __all__ = ["InfraFactory"]
 
 
-class InfraFactory(BaseFactory[str, Tuple[Callable[[Any], Any], bool]]):
+class InfraFactory(BaseFactory[ConfKey, Tuple[Callable[[Any], Any], bool]]):
     @classmethod
-    def register(cls, key: str, need_conf: bool = True, **kwargs):  # type: ignore
+    def register(cls, key: ConfKey, need_conf: bool = True, **kwargs):  # type: ignore
         def decorator(func: Callable[[Any], Any]):
             super(InfraFactory, cls).register(key, **kwargs)((func, need_conf))
             return func
