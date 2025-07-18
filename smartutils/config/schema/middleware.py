37e04181e35ce63962d002d2c95ac23a8a4fb56f
wuruimiao <dev.wuruimiao@qq.com>
2025-07-18 14:04:07 +0800

    refactor: middleware config

--- smartutils/config/schema/middleware.py
+++ smartutils/config/schema/middleware.py
@@ -1,7 +1,7 @@
 from enum import Enum
-from typing import List
+from typing import Dict, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from smartutils.config.const import ConfKey
 from smartutils.config.factory import ConfFactory
@@ -15,6 +15,25 @@ class MiddlewarePluginKey(str, Enum):
     LOG = "log"
 
 
+class PluginMeConf(BaseModel):
+    access_token_name: str = Field(..., description="访问令牌name")
+
+
+class PluginPermissionConf(BaseModel):
+    access_token_name: str = Field(..., description="访问令牌name")
+
+
+class _MiddlewareConfig(BaseModel):
+    me: PluginMeConf = Field(..., description="me插件配置")
+    persmission: PluginPermissionConf = Field(..., description="permission插件配置")
+
+
 @ConfFactory.register(ConfKey.MIDDLEWARE, multi=False, require=False)
 class MiddlewareConf(BaseModel):
-    enable: List[MiddlewarePluginKey]
+    app: Optional[List[MiddlewarePluginKey]] = Field(
+        None, description="启用的全局中间件"
+    )
+    routes: Optional[Dict[str, List[MiddlewarePluginKey]]] = Field(
+        None, description="启用的路由级别的中间件"
+    )
+    config: Optional[_MiddlewareConfig] = Field(None, description="中间件配置")
