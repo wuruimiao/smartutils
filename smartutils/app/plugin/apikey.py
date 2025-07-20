310d2a053a0f11453dd3c5a34ac582b4025bcb14
wuruimiao <dev.wuruimiao@qq.com>
2025-07-21 02:04:58 +0800

    test: apikey

--- smartutils/app/plugin/apikey.py
+++ smartutils/app/plugin/apikey.py
@@ -28,26 +28,24 @@ class ApiKeyPlugin(AbstractMiddlewarePlugin):
         req: RequestAdapter,
         next_adapter: Callable[[], Awaitable[ResponseAdapter]],
     ) -> ResponseAdapter:
-        api_key = req.get_header(HeaderKey(self._conf.api_key.header_key))
-        if api_key not in self._conf.api_key.keys:
+        api_key = req.get_header(self._conf.apikey.header_key)  # type: ignore
+        if api_key not in self._conf.apikey.keys:
             return self._resp_fn(
                 UnauthorizedError(
                     f"ApiKeyPlugin invalid key, received: {api_key}"
                 ).as_dict
             )
 
-        if self._conf.api_key.secret:
-            signature = req.get_header(HeaderKey(self._conf.api_key.header_signature))
-            timestamp = req.get_header(HeaderKey(self._conf.api_key.header_timestamp))
+        if self._conf.apikey.secret:
+            signature = req.get_header(self._conf.apikey.header_signature)  # type: ignore
+            timestamp = req.get_header(self._conf.apikey.header_timestamp)  # type: ignore
             if not signature or not timestamp:
                 return self._resp_fn(
                     UnauthorizedError(
                         "ApiKeyPlugin missing signature or timestamp"
                     ).as_dict
                 )
-            expected_sign = calc_signature(
-                api_key, timestamp, self._conf.api_key.secret
-            )
+            expected_sign = calc_signature(api_key, timestamp, self._conf.apikey.secret)
             if signature != expected_sign:
                 return self._resp_fn(
                     UnauthorizedError("ApiKeyPlugin invalid signature").as_dict
