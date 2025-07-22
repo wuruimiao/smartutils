8021c59358be0076be59b139377bca2a4871dd83
wuruimiao <dev.wuruimiao@qq.com>
2025-07-22 17:46:13 +0800

    ✨ feat(script): add script to find duplicate test names

---
+++ script/find_dup_test_name.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import ast
+import os
+from collections import defaultdict
+
+
+def find_test_functions(pyfile):
+    with open(pyfile, "r", encoding="utf-8") as f:
+        tree = ast.parse(f.read(), pyfile)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef) and node.name.startswith("test_"):
+            yield node.name, pyfile, node.lineno
+
+
+def scan_test_functions(root_dir):
+    test_funcs = defaultdict(list)
+    for dirpath, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith(".py") and not filename.endswith(".pyc"):
+                print(f"scanning {filename}")
+                pyfile = os.path.join(dirpath, filename)
+                for name, file, lineno in find_test_functions(pyfile):
+                    print(f"find {name}")
+                    test_funcs[name].append((file, lineno))
+    return test_funcs
+
+
+if __name__ == "__main__":
+    root = "."  # 项目根目录
+    funcs = scan_test_functions(root)
+    for name, locations in funcs.items():
+        if len(locations) > 1:
+            print(f"重复的测试函数名: {name}")
+            for file, lineno in locations:
+                print(f"  文件: {file}, 行号: {lineno}")
