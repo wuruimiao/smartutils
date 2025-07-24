e0c15f6a0cd8b0cf94f8e420ac6a8d02af7c321b
wuruimiao <dev.wuruimiao@qq.com>
2025-07-24 13:18:56 +0800

    ♻️ refactor(plugin): remove unused import Dict

--- smartutils/app/plugin/common.py
+++ smartutils/app/plugin/common.py
@@ -1,5 +1,5 @@
 import base64
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 from smartutils.app.adapter.req.abstract import RequestAdapter
 from smartutils.app.const import HeaderKey
