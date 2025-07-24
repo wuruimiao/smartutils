f9128025b0c2c763b2e1ac05d332b0b1bb506ab3
wuruimiao <dev.wuruimiao@qq.com>
2025-07-24 13:31:21 +0800

    🐛 fix(scripts): correct argument handling in run_test.sh

--- script/run_test.sh
+++ script/run_test.sh
@@ -2,10 +2,10 @@
 
 # 运行测试并收集代码覆盖率
 
-if [[ "$1" == "real" ]]; then
+if [[ -n "$1" ]]; then
     if [[ -n "$2" ]]; then
         pytest --cov=smartutils --cov-report=xml "$2" --maxfail=1 -v > test_output.log
-    else
+        elif [[ "$1" == "real" ]]; then
         pytest --cov=smartutils --cov-report=xml tests tests_real --maxfail=1 -v > test_output.log
     fi
 else
