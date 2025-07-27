d2f0c77a89320fcac49f7b7f054416711e118a31
wuruimiao <dev.wuruimiao@qq.com>
2025-07-27 22:56:27 +0800

    ♻️ refactor(singleton): centralize singleton instance management

--- smartutils/design/_singleton.py
+++ smartutils/design/_singleton.py
@@ -1,5 +1,7 @@
 import threading
+import weakref
 from functools import wraps
+from typing import Callable, Dict
 
 """
 此单例不可直接/间接在构造中做异步IO或 await 操作。
@@ -8,8 +10,37 @@ from functools import wraps
 
 __all__ = ["singleton", "SingletonBase", "SingletonMeta", "reset_all"]
 
-_singleton_instances = {}
-_singleton_lock = threading.Lock()
+
+class _SingleTonData:
+    REGISTRY = {}
+    LOCKS: Dict[type, threading.Lock] = {}
+    LOCKS_LOCK = threading.Lock()
+
+    @classmethod
+    def get_instance(cls, instance_cls: type, new_instance: Callable):
+        if instance_cls not in cls.LOCKS:
+            with cls.LOCKS_LOCK:
+                if instance_cls not in cls.LOCKS:
+                    cls.LOCKS[instance_cls] = threading.Lock()
+
+        if instance_cls not in cls.REGISTRY:
+            with cls.LOCKS[instance_cls]:
+                if instance_cls not in cls.REGISTRY:
+                    cls.REGISTRY[instance_cls] = new_instance()
+
+        return cls.REGISTRY[instance_cls]
+
+    @classmethod
+    def reset(cls, instance_cls: type):
+        if instance_cls in cls.LOCKS:
+            with cls.LOCKS[instance_cls]:
+                if instance_cls in cls.REGISTRY:
+                    cls.REGISTRY.pop(instance_cls)
+
+    @classmethod
+    def reset_all(cls):
+        for instance_cls in cls.LOCKS.keys():
+            cls.reset(instance_cls)
 
 
 def singleton(cls):
@@ -44,15 +75,10 @@ def singleton(cls):
     # 用 class 装饰器返回了实例（不是类），破坏了类方法的运行机制
     @wraps(cls)
     def get_instance(*args, **kwargs):
-        if cls not in _singleton_instances:
-            with _singleton_lock:
-                if cls not in _singleton_instances:
-                    _singleton_instances[cls] = cls(*args, **kwargs)
-        return _singleton_instances[cls]
+        return _SingleTonData.get_instance(cls, lambda: cls(*args, **kwargs))
 
     def reset():
-        with _singleton_lock:
-            _singleton_instances.pop(cls, None)
+        _SingleTonData.reset(cls)
 
     get_instance.reset = reset  # type: ignore
     return get_instance
@@ -65,19 +91,10 @@ class SingletonBase:
     class MySingleton(SingletonBase): ...
     """
 
-    _instance = None
-    _lock = threading.Lock()
-    _singleton_classes = set()
-
     def __new__(cls, *args, **kwargs):
-        # 注册到单例集合
-        if cls is not SingletonBase:
-            SingletonBase._singleton_classes.add(cls)
-        if not hasattr(cls, "_instance") or cls._instance is None:
-            with cls._lock:
-                if not hasattr(cls, "_instance") or cls._instance is None:
-                    cls._instance = super().__new__(cls)
-        return cls._instance
+        return _SingleTonData.get_instance(
+            cls, lambda: super(SingletonBase, cls).__new__(cls, *args, **kwargs)
+        )
 
     def _init_once(self, *args, **kwargs):
         pass
@@ -90,16 +107,7 @@ class SingletonBase:
 
     @classmethod
     def reset(cls):
-        with cls._lock:
-            cls._instance = None
-
-    @classmethod
-    def reset_all(cls):
-        """
-        重置所有继承自 SingletonBase 的单例实例
-        """
-        for sub_cls in list(cls._singleton_classes):
-            sub_cls.reset()
+        _SingleTonData.reset(cls)
 
 
 class SingletonMeta(type):
@@ -108,37 +116,15 @@ class SingletonMeta(type):
     class MyMetaSingleton(metaclass=SingletonMeta): ...
     """
 
-    _instances = {}
-    _locks = {}
-    _locks_control = threading.Lock()
-
     def __call__(cls, *args, **kwargs):
-        if cls not in SingletonMeta._locks:
-            with SingletonMeta._locks_control:
-                if cls not in SingletonMeta._locks:
-                    SingletonMeta._locks[cls] = threading.Lock()
-        # 后续的加锁只用自己 class 的锁
-        if cls not in SingletonMeta._instances:
-            with SingletonMeta._locks[cls]:
-                if cls not in SingletonMeta._instances:
-                    SingletonMeta._instances[cls] = super().__call__(*args, **kwargs)
-        return SingletonMeta._instances[cls]
+        return _SingleTonData.get_instance(
+            cls, lambda: super(SingletonMeta, cls).__call__(*args, **kwargs)
+        )
 
     def reset(cls):
-        if cls in SingletonMeta._locks:
-            with SingletonMeta._locks[cls]:
-                SingletonMeta._instances.pop(cls, None)
-
-    @classmethod
-    def reset_all(mcs):
-        for cls in list(mcs._instances):
-            if cls in mcs._locks:
-                with mcs._locks[cls]:
-                    mcs._instances.pop(cls, None)
+        _SingleTonData.reset(cls)
 
 
 def reset_all():
-    with _singleton_lock:
-        _singleton_instances.clear()
-    SingletonBase.reset_all()
-    SingletonMeta.reset_all()
+    # 全部清空
+    _SingleTonData.reset_all()
