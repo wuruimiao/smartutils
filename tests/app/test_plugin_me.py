4b702e1ee46941d566fd82a1c2c40b06fda98dac
wuruimiao <dev.wuruimiao@qq.com>
2025-07-28 18:02:30 +0800

    ✅ test(plugin_me): add tests for MePlugin initialization errors

--- tests/app/test_plugin_me.py
+++ tests/app/test_plugin_me.py
@@ -1,14 +1,29 @@
 import pytest
 
+from smartutils.app.const import AppKey, RunEnv
 from smartutils.config.schema.middleware import (
     MiddlewarePluginSetting,
     PluginDependAuthConf,
 )
 from smartutils.error.sys import LibraryUsageError
 
+RunEnv.set_app(AppKey.FASTAPI)
 
-def test_plugin_me_init_exception():
+
+def test_plugin_me_init_no_token_helper():
     from smartutils.app.plugin.me import MePlugin
 
-    with pytest.raises(LibraryUsageError):
+    with pytest.raises(LibraryUsageError) as exc:
         MePlugin(conf=MiddlewarePluginSetting(me=PluginDependAuthConf(local=True)))
+    assert (
+        str(exc.value)
+        == "[MePlugin] requires token in config.yaml. err=[TokenHelper] must init by infra."
+    )
+
+
+def test_plugin_me_init_no_auth_client():
+    from smartutils.app.plugin.me import MePlugin
+
+    with pytest.raises(LibraryUsageError) as exc:
+        MePlugin(conf=MiddlewarePluginSetting(me=PluginDependAuthConf(local=False)))
+    assert str(exc.value) == "[MePlugin] requires auth below client in config.yaml."
