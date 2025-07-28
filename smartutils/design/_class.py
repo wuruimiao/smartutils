ba4e2d1d43054e816b00d1151758c75bb4ce950f
wuruimiao <dev.wuruimiao@qq.com>
2025-07-28 09:52:34 +0800

    ♻️ refactor(design): improve classproperty implementation

--- smartutils/design/_class.py
+++ smartutils/design/_class.py
@@ -1,12 +1,20 @@
-class classproperty(property):
-    def __get__(self, obj, cls):
-        return self.fget(cls)
+class classproperty:
+    """
+    兼容类和实例访问的 classproperty。
+    可通过 MyClass.foo 和 MyClass().foo 访问，效果一致。
+    """
+
+    def __init__(self, fget):
+        self.fget = fget
+
+    def __get__(self, instance, owner):
+        return self.fget(owner)
 
 
 class MyBase:
     @classproperty
     def name(cls):
-        return f"[{cls.__name__}]"
+        return f"[{cls.__name__}]"  # type: ignore
 
     @classproperty
     def full_name(cls):
