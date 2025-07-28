3947da4c02255b813fd50649204c0beee9210a49
wuruimiao <dev.wuruimiao@qq.com>
2025-07-28 16:58:25 +0800

    ✅ test(fastapi): add new tests for fastapi me routes
    ✅ test(fastapi): rename and update permission test file

--- tests/app/test_main_fastapi_routes_me.py
+++ tests/app/test_main_fastapi_routes_me.py
@@ -15,11 +15,6 @@ middleware:
       - exception
       - header
       - me
-    permission:
-      - log
-      - exception
-      - header
-      - permission
 
 client:
   auth:
@@ -80,21 +75,6 @@ project:
 
         app.include_router(me_router)
 
-        permission_router = APIRouter(route_class=mgr.init_route("permission"))
-
-        @permission_router.get("/test-permission")
-        def permission():
-            return ResponseModel(
-                data={
-                    "userid": ReqCTX.get_userid(),
-                    "username": ReqCTX.get_username(),
-                    "traceid": ReqCTX.get_traceid(),
-                    "user_ids": ReqCTX.get_permission_user_ids(),
-                }
-            )
-
-        app.include_router(permission_router)
-
     from smartutils.app.main.fastapi import create_app
 
     app = create_app(str(config_file))
@@ -111,7 +91,7 @@ project:
 
 
 @pytest.fixture
-async def fake_me_permission(mocker):
+async def fake_me(mocker):
     # patch HttpClient._client.request
     async def fake_request(method, url, *args, **kwargs):
         if url.endswith("/me"):
@@ -131,30 +111,6 @@ async def fake_me_permission(mocker):
                     pass
 
             return FakeResp()
-        elif url.endswith("/permission"):
-
-            class FakeResp:
-                status_code = 200
-
-                def json(self):
-                    return {
-                        "code": 0,
-                        "data": {
-                            "can_access": True,
-                            "user_ids": [1, 2, 3],
-                            "no permission": "",
-                        },
-                    }
-
-                async def aread(self):
-                    from json import dumps
-
-                    return dumps(self.json()).encode("utf-8")
-
-                async def aclose(self):
-                    pass
-
-            return FakeResp()
         else:
             raise Exception("不对!!!")
 
@@ -164,7 +120,7 @@ async def fake_me_permission(mocker):
     yield
 
 
-async def test_routes_no_plugin_success(client):
+async def test_routes_me_no_plugin_success(client):
     resp = client.get("/no-plugin")
     data = resp.json()
     assert resp.status_code == 200
@@ -174,7 +130,7 @@ async def test_routes_no_plugin_success(client):
     assert data["data"]["user_ids"] is None
 
 
-async def test_routes_me_middleware_success(client, fake_me_permission):
+async def test_routes_me_middleware_success(client, fake_me):
     resp = client.get("/test-me", cookies={"access_token": "fake"})
     data = resp.json()
     assert resp.status_code == 200
@@ -182,13 +138,3 @@ async def test_routes_me_middleware_success(client, fake_me_permission):
     assert data["data"]["username"] == "unit"
     assert data["data"]["traceid"] != ""
     assert data["data"]["user_ids"] is None
-
-
-async def test_routes_permission_middleware_success(client, fake_me_permission):
-    resp = client.get("/test-permission", cookies={"access_token": "fake"})
-    data = resp.json()
-    assert resp.status_code == 200
-    assert data["data"]["userid"] == 0
-    assert data["data"]["username"] == ""
-    assert data["data"]["traceid"] != ""
-    assert data["data"]["user_ids"] == [1, 2, 3]
