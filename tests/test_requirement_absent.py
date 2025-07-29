54e3076ae293748c8755c9d62637f895c6966423
wuruimiao <dev.wuruimiao@qq.com>
2025-07-29 13:12:14 +0800

    ✅ test(requirement_absent): rename and add missing dependency tests
    ✨ test(requirement_absent): add new tests for missing dependencies

--- tests/test_requirement_absent.py
+++ tests/test_requirement_absent.py
@@ -22,49 +22,81 @@ def reset(mocker):
     mocker.patch.object(BaseFactory, "register", classmethod(mocked_register))
 
 
-def test_importerror_when_httpx_missing(mock_module_absent, reset):
+def test_client_http_missing(mock_module_absent, reset):
     import smartutils.infra.client.http as mod
 
     mock_module_absent("httpx", mod=mod)
 
     with pytest.raises(LibraryUsageError) as exc:
-        from smartutils.infra.client.http import HttpClient
-
-        HttpClient(conf=1, name="demo")
+        mod.HttpClient(conf=1, name="demo")
     assert str(exc.value) == "[HttpClient] depend on httpx, install first!"
 
 
-def test_tokenhelper_missing_jwt(mock_module_absent, reset):
+def test_tokenhelper_missing(mock_module_absent, reset):
     import smartutils.app.auth.token as mod
 
     mock_module_absent("jwt", mod=mod)
 
     with pytest.raises(LibraryUsageError) as exc:
-        from smartutils.app.auth.token import TokenHelper
-
-        TokenHelper(conf=1)
+        mod.TokenHelper(conf=1)
     assert str(exc.value) == "[TokenHelper] depend on jwt, install first!"
 
 
-def test_redis_missing_redis(mock_module_absent, reset):
+def test_redis_missing(mock_module_absent, reset):
     import smartutils.infra.cache.redis as mod
 
     mock_module_absent("redis", "redis.asyncio", mod=mod)
 
     with pytest.raises(LibraryUsageError) as e:
-        from smartutils.infra.cache.redis import AsyncRedisCli
-
-        AsyncRedisCli(conf=1, name="failcli")
+        mod.AsyncRedisCli(conf=1, name="failcli")
     assert str(e.value) == "[AsyncRedisCli] depend on redis, install first!"
 
 
-def test_assert_mongo_missing_motor(mock_module_absent, reset):
+def test_mongo_missing(mock_module_absent, reset):
     import smartutils.infra.db.mongo as mod
 
     mock_module_absent("motor", "motor.motor_asyncio", mod=mod)
 
     with pytest.raises(LibraryUsageError) as e:
-        from smartutils.infra.db.mongo import AsyncMongoCli
+        mod.AsyncMongoCli(conf=1, name="abc")
+    assert str(e.value) == "[AsyncMongoCli] depend on motor, install first!"
 
-        AsyncMongoCli(conf=1, name="abc")
-    assert "depend on motor" in str(e.value)
+
+def test_password_missing(mock_module_absent, reset):
+    import smartutils.app.auth.password as mod
+
+    mock_module_absent("bcrypt", mod=mod)
+
+    with pytest.raises(LibraryUsageError) as e:
+        mod.PasswordHelper()
+    assert str(e.value) == "[PasswordHelper] depend on bcrypt, install first!"
+
+
+def test_otp_missing(mock_module_absent, reset):
+    import smartutils.app.auth.otp as mod
+
+    mock_module_absent("pyotp", "qrcode", mod=mod)
+
+    with pytest.raises(LibraryUsageError) as e:
+        mod.OtpHelper()
+    assert str(e.value) == "[OtpHelper] depend on pyotp, qrcode, install first!"
+
+
+def test_missing_fastapi(mock_module_absent, reset):
+    import smartutils.app.adapter.req.starlette as mod
+
+    mock_module_absent("fastapi", mod=mod)
+
+    mod.StarletteRequestAdapter(1)
+
+    import smartutils.app.adapter.resp.starlette as mod
+
+    mock_module_absent("fastapi", mod=mod)
+    mod.StarletteResponseAdapter(1)
+
+
+# def test_missing_sqlalchemy(mock_module_absent, reset):
+#     import smartutils.app.history.model as mod
+
+#     mock_module_absent("sqlalchemy", mod=mod)
+#     assert mod.OpType.ADD == 1
