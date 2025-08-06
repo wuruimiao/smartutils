77c6045b6ec48ef092fe9d998876616c369368e1
wuruimiao <dev.wuruimiao@qq.com>
2025-08-06 13:41:13 +0800

    🔥 chore(design): remove unused imports

--- smartutils/design/__init__.py
+++ smartutils/design/__init__.py
@@ -1,8 +1,6 @@
 from smartutils.design._class import MyBase
 from smartutils.design._singleton import SingletonBase, SingletonMeta, singleton
 from smartutils.design.attr import RequireAttrs
-from smartutils.design.container.abstract_pri import PriContainer
-from smartutils.design.container.pri_dict_list import PriContainerDictList
 from smartutils.design.deprecated import deprecated
 from smartutils.design.factory import BaseFactory
 from smartutils.design.module import require_modules
@@ -16,6 +14,4 @@ __all__ = [
     "RequireAttrs",
     "require_modules",
     "MyBase",
-    "PriContainer",
-    "PriContainerDictList",
 ]
