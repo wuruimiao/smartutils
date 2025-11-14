f197a9e728d8aa388f16e32865022750ea16484f
wuruimiao <dev.wuruimiao@qq.com>
2025-11-14 15:57:25 +0800

    ♻️ refactor(cache): inject DecodeBytes dependency into SafeString
    ♻️ refactor(cache): update num_op, incr, decr to return int

--- smartutils/infra/cache/string.py
+++ smartutils/infra/cache/string.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from enum import Enum
 from typing import TYPE_CHECKING, Optional
 
+from smartutils.infra.cache.decode import DecodeBytes
 from smartutils.infra.cache.lua_manager import LuaManager, LuaName
 
 try:
@@ -19,10 +20,11 @@ class LuaOp(str, Enum):
 
 
 class SafeString:
-    def __init__(self, redis_cli: Redis):
+    def __init__(self, redis_cli: Redis, decode_bytes: DecodeBytes):
         self._redis: Redis = redis_cli
+        self._decode_bytes = decode_bytes
 
-    async def _num_op(self, op: LuaOp, key: str, ex: Optional[int] = None) -> str:
+    async def _num_op(self, op: LuaOp, key: str, ex: Optional[int] = None) -> int:
         """
         执行指定op的原子自增/自减操作
         :param op: StringOp.INCR or StringOp.DECR
@@ -31,15 +33,15 @@ class SafeString:
         ex_str = str(ex) if ex else ""
         return await LuaManager.call(
             LuaName.INCR_DECR, self._redis, keys=[key], args=[op.value, ex_str]
-        )
+        )  # type: ignore
 
-    async def incr(self, key: str, ex: Optional[int] = None) -> str:
+    async def incr(self, key: str, ex: Optional[int] = None) -> int:
         """
         原子自增计数器。
         """
         return await self._num_op(LuaOp.INCR, key, ex)
 
-    async def decr(self, key: str, ex: Optional[int] = None) -> str:
+    async def decr(self, key: str, ex: Optional[int] = None) -> int:
         """
         原子自减计数器。
         """
