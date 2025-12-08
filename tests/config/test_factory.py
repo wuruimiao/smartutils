a1948e960d80654ac963154da6b8f432be6cd6d4
wuruimiao <dev.wuruimiao@qq.com>
2025-12-08 16:12:44 +0800

    ♻️ refactor(test_factory): use explicit keyword arguments for ConfMeta

--- tests/config/test_factory.py
+++ tests/config/test_factory.py
@@ -10,7 +10,7 @@ def test_conf_factory_create_single():
     class DummyConf(BaseModel):
         foo: int
 
-    ConfFactory.register(ConfKey("__test__"), meta=ConfMeta(False, True))(
+    ConfFactory.register(ConfKey("__test__"), meta=ConfMeta(multi=False, require=True))(
         DummyConf
     )  # 类型强制转换
     conf = ConfFactory.create(ConfKey("__test__"), {"foo": 1})  # 类型强制转换
@@ -25,7 +25,9 @@ def test_conf_factory_create_missing_required():
     class DummyConf(BaseModel):
         foo: int
 
-    ConfFactory.register(ConfKey("__test__"), meta=ConfMeta(False, True))(DummyConf)
+    ConfFactory.register(ConfKey("__test__"), meta=ConfMeta(multi=False, require=True))(
+        DummyConf
+    )
     with pytest.raises(ConfigError):
         ConfFactory.create(ConfKey("__test__"), {})
     ConfFactory.reset(ConfKey("__test__"))
@@ -35,7 +37,9 @@ def test_conf_factory_create_missing_not_required():
     class DummyConf2(BaseModel):
         foo: int
 
-    ConfFactory.register(ConfKey("__test2__"), meta=ConfMeta(False, False))(DummyConf2)
+    ConfFactory.register(
+        ConfKey("__test2__"), meta=ConfMeta(multi=False, require=False)
+    )(DummyConf2)
     result = ConfFactory.create(ConfKey("__test2__"), {})
     assert result is None
     ConfFactory.reset(ConfKey("__test2__"))
@@ -45,7 +49,9 @@ def test_conf_factory_create_multi():
     class DummyConf3(BaseModel):
         foo: int
 
-    ConfFactory.register(ConfKey("__test3__"), meta=ConfMeta(True, True))(DummyConf3)
+    ConfFactory.register(ConfKey("__test3__"), meta=ConfMeta(multi=True, require=True))(
+        DummyConf3
+    )
     conf = ConfFactory.create(ConfKey("__test3__"), {"default": {"foo": 2}})
     assert conf
     assert isinstance(conf, dict)
@@ -58,7 +64,9 @@ def test_conf_factory_create_multi_missing_group_default():
     class DummyConf4(BaseModel):
         foo: int
 
-    ConfFactory.register(ConfKey("__test4__"), meta=ConfMeta(True, True))(DummyConf4)
+    ConfFactory.register(ConfKey("__test4__"), meta=ConfMeta(multi=True, require=True))(
+        DummyConf4
+    )
     # with pytest.raises(ConfigError):
     ConfFactory.create(ConfKey("__test4__"), {"notdefault": {"foo": 3}})
     ConfFactory.reset(ConfKey("__test4__"))
@@ -68,7 +76,9 @@ def test_conf_factory_create_invalid_conf():
     class DummyConf(BaseModel):
         foo: int
 
-    ConfFactory.register(ConfKey("__test__"), meta=ConfMeta(False, True))(DummyConf)
+    ConfFactory.register(ConfKey("__test__"), meta=ConfMeta(multi=False, require=True))(
+        DummyConf
+    )
     with pytest.raises(ConfigError):
         ConfFactory.create(ConfKey("__test__"), {"foo": "notint"})
     ConfFactory.reset(ConfKey("__test__"))
