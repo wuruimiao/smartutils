d8ab4b657c3526dca23a81278f9dd3dbdee2b4e3
wuruimiao <dev.wuruimiao@qq.com>
2025-12-08 16:26:56 +0800

    ♻️ refactor(config): reorder imports and move __all__ definition

--- smartutils/config/__init__.py
+++ smartutils/config/__init__.py
@@ -1,11 +1,10 @@
+from smartutils.call import register_package
+from smartutils.config import schema
 from smartutils.config._config import Config
 from smartutils.config.const import ConfKey
 from smartutils.config.factory import ConfFactory
 from smartutils.config.schema.project import ProjectConf
 
-__all__ = ["Config", "ConfFactory", "ConfKey", "ProjectConf"]
-
-from smartutils.call import register_package
-from smartutils.config import schema
-
 register_package(schema)
+
+__all__ = ["Config", "ConfFactory", "ConfKey", "ProjectConf"]
