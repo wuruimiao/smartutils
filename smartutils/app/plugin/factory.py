207c32bfb4ab0ea14fa8d7242f648d7046e54575
wuruimiao <dev.wuruimiao@qq.com>
2025-12-08 16:02:45 +0800

    📝 docs(factory): add class docstring for middleware plugin factory

--- smartutils/app/plugin/factory.py
+++ smartutils/app/plugin/factory.py
@@ -9,4 +9,9 @@ __all__ = ["MiddlewarePluginFactory"]
 
 class MiddlewarePluginFactory(
     BaseFactory[MiddlewarePluginKey, Type[AbstractMiddlewarePlugin], None]
-): ...
+):
+    """
+    管理：不同应用框架下，如何构造对应的中间件插件类，以在应用中统一添加中间件插件。
+    """
+
+    ...
